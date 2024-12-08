064b43692f192e3105ccfab30e0c2fb56c8e63ff
Wildan Belfiore <73292455+wildan190@users.noreply.github.com>
2024-12-08 19:05:30 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  // Ambil token dari cookie
+  const token = request.cookies.get('token');
+
+  // Izinkan akses ke halaman register jika token tidak ada
+  if (!token && request.nextUrl.pathname === '/register') {
+    return NextResponse.next();
+  }
+
+  // Redirect ke halaman login jika tidak ada token dan bukan halaman login atau register
+  if (!token && request.nextUrl.pathname !== '/login' && request.nextUrl.pathname !== '/register') {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/products/:path*', '/login', '/register'],
+};
 
